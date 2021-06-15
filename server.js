c29f7543a3fdf4dd9ea73583a3af4e1eeb34f1ba
MJfenix <mandeep.jain999@gmail.com>
2021-06-15 19:05:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false}))
 app.use(flash())
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'hi',
   resave: false,
   saveUninitialized: false
 }))
